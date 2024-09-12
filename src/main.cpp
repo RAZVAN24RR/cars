5e88b455c3a9110878e3357df9b36439df761f85
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-09-12 22:39:36 +0300

    setup complete - "message": "Hello, World!"

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+
+void handle_get(http_request request) {
+    ucout << "Received a GET request\n";
+    json::value response_data;
+    response_data[U("message")] = json::value::string(U("Hello, World!"));
+
+    request.reply(status_codes::OK, response_data);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    uri_builder uri(U("http://localhost:3001"));
+    auto addr = uri.to_uri().to_string();
+    http_listener listener(addr);
+
+    listener.support(methods::GET, handle_get);
+
+    try {
+        listener
+            .open()
+            .then([&listener]() { ucout << "Starting to listen at: " << listener.uri().to_string() << std::endl; })
+            .wait();
+
+        std::string line;
+        std::getline(std::cin, line);
+    } catch (const std::exception &e) {
+        std::cerr << "Something went wrong: " << e.what() << std::endl;
+    }
+
     return 0;
 }
